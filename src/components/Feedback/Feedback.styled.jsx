7d53b395031c2bd15312378c30b7b6c83aaa3f62
Timur.Vdovichenko <timur.vdovichenko@gmail.com>
2023-05-28 15:24:48 +0300

    styles

--- src/components/Feedback/Feedback.styled.jsx
+++ src/components/Feedback/Feedback.styled.jsx
@@ -3,11 +3,13 @@ import styled from 'styled-components';
 export const ButtonStyled = styled.button`
   background: transparent;
   border-radius: 3px;
-  border: 2px solid #bf4f74;
-  color: #bf4f74;
+  border: 1px solid black;
+  color: black;
   margin: 0 0.5em;
   padding: 0.25em 1em;
   text-transform: capitalize;
+  box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14),
+    0px 2px 1px -1px rgba(0, 0, 0, 0.12);
 `;
 
 export const ListStyled = styled.ul`
--- src/components/PhoneBook/Phonebook.styled.jsx
+++ src/components/PhoneBook/Phonebook.styled.jsx
@@ -29,11 +29,13 @@ export const Span = styled.span`
 export const Button = styled.button`
   background: transparent;
   border-radius: 3px;
-  border: 2px solid #bf4f74;
-  color: #bf4f74;
+  border: 1px solid black;
+  color: black;
   padding: 0.25em 1em;
   text-transform: capitalize;
   ${props => props.buttonListStyle};
+  box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14),
+    0px 2px 1px -1px rgba(0, 0, 0, 0.12);
 `;
 
 export const List = styled.ul`
